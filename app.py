d40ca15ed644d72f278ff8aeae33bbfbe6d5f6aa
Cangemi <65517004+Cangemi@users.noreply.github.com>
2022-11-12 09:49:00 -0300

    app.run

--- app.py
+++ app.py
@@ -3,25 +3,23 @@
 # pip shell
 from flask import Flask
 from flask_cors import CORS
+
 from api.cliente_service import cliente
-from api.device_service import  device
-import os
+from api.device_service import device
 
 app = Flask(__name__)
-CORS(app,resources={r"/*":{"origins":"*"}})
+CORS(app, resources={r"/*": {"origins": "*"}})
+
 
 #
 # REGISTRAR AS ROTAS
 #
 app.register_blueprint(cliente,url_prefix='/api/cliente')
 app.register_blueprint(device,url_prefix='/api/device')
-#
-# OPERAÇÕES
-#
-@app.route("/")
-def info():
-    return "API v.1.0"
+
+@app.route('/')
+def hello():
+    return "API Controle de Clientes"
 
 if __name__ == "__main__":
-    port = int(os.getenv('PORT'))
-    app.run(debug=True, host='0.0.0.0',port= port)
+    app.run(host='0.0.0.0', port=5000)
